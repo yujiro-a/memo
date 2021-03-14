87b1c56a187be2f919d71833e84274a7819a3c0b
EC2 Default User <ec2-user@ip-172-31-11-215.us-east-2.compute.internal>
2021-03-14 04:05:27 +0000

    rubytask-memo

---
+++ memo.rb
@@ -0,0 +1,34 @@
+require "csv"
+
+while true do
+  puts "1(新規でメモを作成) 2(既存のメモを編集する)"
+  num = gets.chomp
+  
+  if num == "1" 
+    puts "拡張子を除いたファイル名を入力してください。"
+    text = gets.chomp
+    puts "メモしたい内容を記入してください。"
+    puts "完了したらCtrl + Dを押します。"
+  
+    CSV.open("#{text}.txt","w") do |memo|
+      while memo_text = gets
+        memo << [memo_text.chomp]
+      end
+      exit
+    end
+    
+  elsif num == "2"
+    puts "拡張子を除いたファイル名を入力してください。"
+    text = gets.chomp
+    puts "メモしたい内容を記入してください。"
+    puts "完了したらCtrl + Dを押します。"
+    CSV.open("#{text}.txt","a") do |memo|
+      while memo_text = gets
+        memo << [memo_text.chomp]
+      end
+      exit
+    end
+  else
+    puts "[ERROR]\n入力された数字が間違っています。\n「１」か「２」を入力してください。\n "
+  end
+end
